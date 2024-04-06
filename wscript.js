228a9dcbb287bc16e9d6e3bcb09cafa56e7aea0e
Luis Perez <luisperez07182@email.com>
2024-04-06 03:04:37 -0500

    prob1 unfinished

---
+++ wscript.js
@@ -0,0 +1,40 @@
+document.addEventListener("submit", async function(){
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   // const cityInput = document.getElementById("city").value
+   // const apiKey = "42aa535f489746119a972823240604"
+    // const apiURL = "https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityInput}";
